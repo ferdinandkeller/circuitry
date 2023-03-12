13b9d2eeadbbe18636e890e279e77e43d135cd57
Ferdinand Keller <ferdinand.keller@proton.me>
2023-03-12 23:12:33 +0100

    improved documentation on viewbox global

--- src/globals/viewbox.ts
+++ src/globals/viewbox.ts
@@ -1,4 +1,7 @@
 import { Vector } from '@/utils/vector'
 
-// the viewbox position represents the top left corner of the visible area
+/**
+ * The viewbox position represents the top left corner of the visible area.
+ * The viewbox size is equal to the canvas size.
+ */
 export let viewbox_pos = new Vector(0, 0)
