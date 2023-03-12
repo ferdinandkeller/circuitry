ccb470bf745593755b07c3ebdd9bb7f0f33ed916
Ferdinand Keller <ferdinand.keller@proton.me>
2023-03-12 23:38:09 +0100

    improved utils/math documentation

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,19 +1,39 @@
-// define a method to ceil a number to a multiple of another number
+/**
+ * Ceils a number to a multiple of another number.
+ * 
+ * @param num The number to ceil.
+ * @param modulo The modulo precision.
+ */
 export function modulo_ceiling(num: number, modulo: number): number {
     return Math.ceil(num / modulo) * modulo
 }
 
-// define a method to round a number to a multiple of another number
+/**
+ * Round a number to a multiple of another number.
+ * 
+ * @param num The number to round.
+ * @param modulo The modulo precision.
+ */
 export function modulo_rounding(num: number, modulo: number): number {
     return Math.round(num / modulo) * modulo
 }
 
-// define a method to floor a number to a multiple of another number
+/**
+ * Floors a number to a multiple of another number.
+ * 
+ * @param num The number to floor.
+ * @param modulo The modulo precision.
+ */
 export function modulo_flooring(num: number, modulo: number): number {
     return Math.floor(num / modulo) * modulo
 }
 
-// define a struct modulo function
+/**
+ * Modulo function that always returns a positive number.
+ * 
+ * @param num The number to modulo.
+ * @param modulo The modulo precision.
+ */
 export function mod(num: number, modulo: number): number {
     return ((num % modulo) + modulo) % modulo
 }
