ce503060b9a8582c1ddf48996a081ad20065fffe
Ferdinand Keller <ferdinand.keller@proton.me>
2023-03-13 13:02:29 +0100

    reindent main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,18 @@ import { connect_start, connect_move, connect_end } from './modes/connect'
 import { update_cursor_pos } from './globals/cursor'
 
 /**
- * A full render is a render of the entire canvas.
- * This is used when the window is resized or at startup.
- * 
- * It will resize all the canvas, regenerate the static background, and re-render the background.
- */
+* A full render is a render of the entire canvas.
+* This is used when the window is resized or at startup.
+* 
+* It will resize all the canvas, regenerate the static background, and re-render the background.
+*/
 function full_render() {
-  // resize the canvas to fit the window
-  resize_canvas()
-  // render the static background
-  render_static_background()
-  // render the static background on the visible canvas
-  render_background()
+    // resize the canvas to fit the window
+    resize_canvas()
+    // render the static background
+    render_static_background()
+    // render the static background on the visible canvas
+    render_background()
 }
 
 // whenever the window is resized, re-render the background
@@ -32,32 +32,32 @@ window.addEventListener('resize', full_render)
 full_render()
 
 /**
- * Register mouse events on the renderer.
- * This is because this element contains the canvas and it might not be the same size as the window.
- */
+* Register mouse events on the renderer.
+* This is because this element contains the canvas and it might not be the same size as the window.
+*/
 renderer.addEventListener('mousedown', (mouse_event: MouseEvent) => {
-  // update the mouse's position
-  update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
-  
-  // update depending on the active edition mode
-  if (active_edition_mode === EditionMode.PAN) pan_start()
-  else if (active_edition_mode === EditionMode.CONNECT) connect_start()
+    // update the mouse's position
+    update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
+    
+    // update depending on the active edition mode
+    if (active_edition_mode === EditionMode.PAN) pan_start()
+    else if (active_edition_mode === EditionMode.CONNECT) connect_start()
 })
 
 renderer.addEventListener('mousemove', (mouse_event: MouseEvent) => {
-  // update the mouse's position
-  update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
-  
-  // update depending on the active edition mode
-  if (active_edition_mode === EditionMode.PAN) pan_move()
-  else if (active_edition_mode === EditionMode.CONNECT) connect_move()
+    // update the mouse's position
+    update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
+    
+    // update depending on the active edition mode
+    if (active_edition_mode === EditionMode.PAN) pan_move()
+    else if (active_edition_mode === EditionMode.CONNECT) connect_move()
 })
 
 renderer.addEventListener('mouseup', (mouse_event: MouseEvent) => {
-  // update the mouse's position
-  update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
-  
-  // update depending on the active edition mode
-  if (active_edition_mode === EditionMode.PAN) pan_end()
-  else if (active_edition_mode === EditionMode.CONNECT) connect_end()
+    // update the mouse's position
+    update_cursor_pos(mouse_event.clientX, mouse_event.clientY)
+    
+    // update depending on the active edition mode
+    if (active_edition_mode === EditionMode.PAN) pan_end()
+    else if (active_edition_mode === EditionMode.CONNECT) connect_end()
 })
